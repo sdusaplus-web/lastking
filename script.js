f2e05ba5d949fea46ed44a53888d4a25f7f9fa04
sdusaplus-web <sdusaplus@gmail.com>
2025-10-21 13:30:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        resumeBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+    });
+});
